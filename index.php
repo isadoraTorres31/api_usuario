afa003b47b0cd8a3f72f1a83243e82cbd8cadc23
isaTorre31 <isadoradamaiatorres@gmail.com>
2025-04-01 16:37:58 -0300

    atualizado

--- index.php
+++ index.php
@@ -1,60 +1,58 @@
 <?php
- 
-header("Content-Type: application/json");
- 
-$method = $_SERVER['REQUEST_METHOD'];
-$data = json_decode(file_get_contents("php://input"), true);
- 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Base de dados em memória
 $usuarios = [
-    [ "id" => 1, "login" => "user1", "senha" => "1234", "nome" => "Usuário 1", "perfil" => "admin" ],
-    [ "id" => 2, "login" => "user2", "senha" => "1234", "nome" => "Usuário 2", "perfil" => "user" ],
-    [ "id" => 3, "login" => "user3", "senha" => "1234", "nome" => "Usuário 3", "perfil" => "user" ],
-    [ "id" => 4, "login" => "user4", "senha" => "1234", "nome" => "Usuário 4", "perfil" => "user" ],
-    [ "id" => 5, "login" => "user5", "senha" => "1234", "nome" => "Usuário 5", "perfil" => "user" ]
+    ["id" => 1, "login" => "user1", "senha" => "1234", "nome" => "Usuário 1", "perfil" => "admin"],
+    ["id" => 2, "login" => "user2", "senha" => "1234", "nome" => "Usuário 2", "perfil" => "user"],
+    ["id" => 3, "login" => "user3", "senha" => "1234", "nome" => "Usuário 2", "perfil" => "user"],
+    ["id" => 4, "login" => "user4", "senha" => "1234", "nome" => "Usuário 2", "perfil" => "user"],
+    ["id" => 5, "login" => "user5", "senha" => "1234", "nome" => "Usuário 2", "perfil" => "user"],
+    // ... outros usuários
 ];
- 
-if ($method == 'POST') {
+
+// Rota GET para listar usuários
+$app->get('/usuarios', function (Request $request, Response $response) use ($usuarios) {
+    $response->getBody()->write(json_encode(array_slice($usuarios, 0, 5)));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+// Rota POST para criar usuário
+$app->post('/usuarios', function (Request $request, Response $response) use (&$usuarios) {
+    $data = $request->getParsedBody();
+    
     if (!isset($data['login']) || !isset($data['senha'])) {
-        echo json_encode(["erro" => "Login e senha são obrigatórios"]);
-        http_response_code(400);
-        exit;
+        $response->getBody()->write(json_encode(["erro" => "Login e senha são obrigatórios"]));
+        return $response->withStatus(400);
     }
-   
+    
     $novoUsuario = [
         "id" => count($usuarios) + 1,
         "login" => $data['login'],
         "senha" => $data['senha'],
         "nome" => $data['nome'] ?? '',
-        "perfil" => $data['perfil'] ?? ''
+        "perfil" => $data['perfil'] ?? 'user'
     ];
-   
+    
     $usuarios[] = $novoUsuario;
-    echo json_encode($novoUsuario);
-    http_response_code(201);
-}
- 
-elseif ($method == 'GET') {
-    echo json_encode(array_slice($usuarios, 0, 5));
-}
- 
-elseif ($method == 'DELETE') {
-    parse_str($_SERVER['QUERY_STRING'], $query);
-    if (!isset($query['id'])) {
-        echo json_encode(["erro" => "ID é obrigatório"]);
-        http_response_code(400);
-        exit;
-    }
-   
-    $id = (int) $query['id'];
+    $response->getBody()->write(json_encode($novoUsuario));
+    return $response->withStatus(201);
+});
+
+// Rota DELETE para remover usuário
+$app->delete('/usuarios/{id}', function (Request $request, Response $response, array $args) use (&$usuarios) {
+    $id = (int)$args['id'];
     $usuarios = array_filter($usuarios, fn($user) => $user['id'] !== $id);
-   
-    echo json_encode(["mensagem" => "Usuário excluído com sucesso"]);
-    http_response_code(200);
-}
- 
-else {
-    echo json_encode(["erro" => "Método não permitido"]);
-    http_response_code(405);
-}
-?>
+    
+    $response->getBody()->write(json_encode(["mensagem" => "Usuário excluído com sucesso"]));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
  
